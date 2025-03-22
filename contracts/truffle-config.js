1bc77525da271475dfada0b29a71af0ea46d036f
Yosgi <siqi.liu@nextspace.com>
2025-03-23 10:59:57 +1300

    contact update

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -112,12 +112,12 @@ module.exports = {
     },
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
      network_id: "5777",       // Any network (default: none)
     },
     ganache: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*", // 适配所有 Network ID
     },
     //
